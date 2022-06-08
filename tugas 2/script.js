c70a0dc16485d6db0b678f9852c48422201e610c
valdz13 <valdi.haris@ti.ukdw.ac.id>
2022-06-08 18:48:02 +0700

    revisi

--- tugas 2/script.js
+++ tugas 2/script.js
@@ -1,33 +1,72 @@
+const yellow = "yellow";
+const blue = "blue";
+const wrapper = document.getElementById("wrapper");
+const boxElements = document.querySelectorAll(".box");
+const resetButton = document.getElementById("reset");
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-const cross = 'cross';
-const circle = 'circle';
-
-const boxElement = document.querySelectorAll('.box')
-const resetButton = document.getElementById('reset')
-
-let turn = cross;
+let turn = yellow;
 
 resetButton.onclick = startGame;
 
-
-
-function handleClick(e){
-    const boxTarget = e.target;
-
-    if(turn === cross){
-    boxTarget.style.backgroundColor = 'yellow';
-    turn = circle;
-    }else{
-        boxTarget.style.backgroundColor = 'blue';
-        turn = cross;
-    }
+function checkWin() {
+  return winningCombinations.some((combination) => {
+    return combination.every((index) => {
+      return boxElements[index].classList.contains(turn);
+    });
+  });
 }
 
-function startGame(){
-    boxElement.forEach(box =>{
-        box.style.backgroundColor = 'white';
-        box.addEventListener("click", handleClick, {once:true});
-    });
+function checkDraw() {
+  return [...boxElements].every((box) => {
+    return box.classList.contains(yellow) || box.classList.contains(blue);
+  });
+}
+
+function placeMark(box, currentClass) {
+  box.classList.add(currentClass);
+}
+
+function handleTurn() {
+  turn = turn !== yellow ? yellow : blue;
+}
+
+function endGame(){
+  boxElements.forEach(box => {
+    box.removeEventListener("click", handleClick, { once: true });
+  });
+}
+
+function handleClick(e) {
+  const boxTarget = e.target;
+
+  placeMark(boxTarget, turn);
+  if (checkWin()) {
+    console.log(`winner is ${turn}`);
+    wrapper.append(`winner is ${turn}`)
+    endGame();
+  } else if (checkDraw()) {
+    console.log(`the game is draw`);
+  }
+
+  handleTurn();
+}
+
+function startGame() {
+  boxElements.forEach((box) => {
+    box.classList.remove(yellow);
+    box.classList.remove(blue);
+    box.addEventListener("click", handleClick, { once: true });
+  });
 }
 
 startGame();
